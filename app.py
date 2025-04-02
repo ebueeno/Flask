fb8b7c37aada9695f94814f70a9bd4216ead22bf
root <root@ip-172-31-14-240.sa-east-1.compute.internal>
2025-04-02 19:03:35 +0000

    feat: CRUD adicionado

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, flask"
+
+items = []
+@app.route('/items', methods=['GET'])
+def get_imtes():
+    return jsonify(items) #Retorna a lista como um Json
+
+@app.route('/items', methods=['POST'])
+def create_item():
+    data = request.get_json()
+    items.append(data)
+    return jsonify(data), 201
+
+@app.route('/items/<int:item_id>',methods=['PUT'])
+def update_item(item_id): #identifica o item pelo indice<int:item_id>
+    data = request.get_json()
+    if 0 <= item_id <len(items): # Valida de o id está em um intervalo valido 
+        items[item_id].update(data)
+        return jsonify(items[item_id])
+    return jsonify({'error':'Item not found'}), 404
+
+@app.route('/items/<int:item_id>',methods=['DELETE'])
+def delete_item(item_id):
+    if 0 <= item_id < len (items):
+        removed = items.pop(item_id)
+        return jsonify(removed)
+    return jsonify({"error":"Item not found"}),404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
