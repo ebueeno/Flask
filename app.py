aed298fd961097f5ba8e434387464b9a8bd09ed9
root <root@ip-172-31-14-240.sa-east-1.compute.internal>
2025-04-05 02:57:19 +0000

    ajust

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from flasgger import Swagger
 import requests
@@ -7,37 +7,44 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 app.config['SWAGGER'] = {
-    'title':'My flask API',
-    'uiversion':3
+    'title': 'My flask API',
+    'uiversion': 3
 }
 
 swagger = Swagger(app)
 
-users ={
-    "user1":"senha1",
-    "user2":"senha2"
+users = {
+    "user1": "senha1",
+    "user2": "senha2"
 }
 
 auth = HTTPBasicAuth()
 
+
 @auth.verify_password
-def verify_password(username,password):
+def verify_password(username, password):
     if username in users and users[username] == password:
         return username
+
 
 @app.route('/')
 def home():
     return "Hello, flask"
 
-@app.route('/hello',methods=['GET'])
+
+@app.route('/hello', methods=['GET'])
 @auth.login_required
 def hello():
-    return jsonify({"message":"Hello, World!"})
+    return jsonify({"message": "Hello, World!"})
+
 
 items = []
+
+
 @app.route('/items', methods=['GET'])
 def get_imtes():
-    return jsonify(items) #Retorna a lista como um Json
+    return jsonify(items)  # Retorna a lista como um Json
+
 
 @app.route('/items', methods=['POST'])
 def create_item():
@@ -45,34 +52,37 @@ def create_item():
     items.append(data)
     return jsonify(data), 201
 
-@app.route('/items/<int:item_id>',methods=['PUT'])
-def update_item(item_id): #identifica o item pelo indice<int:item_id>
+
+@app.route('/items/<int:item_id>', methods=['PUT'])
+def update_item(item_id):  # identifica o item pelo indice<int:item_id>
     data = request.get_json()
-    if 0 <= item_id <len(items): # Valida de o id está em um intervalo valido 
+    if 0 <= item_id < len(items):  # Valida de o id está em um intervalo valido
         items[item_id].update(data)
         return jsonify(items[item_id])
-    return jsonify({'error':'Item not found'}), 404
+    return jsonify({'error': 'Item not found'}), 404
 
-@app.route('/items/<int:item_id>',methods=['DELETE'])
+
+@app.route('/items/<int:item_id>', methods=['DELETE'])
 def delete_item(item_id):
-    if 0 <= item_id < len (items):
+    if 0 <= item_id < len(items):
         removed = items.pop(item_id)
         return jsonify(removed)
-    return jsonify({"error":"Item not found"}),404
+    return jsonify({"error": "Item not found"}), 404
 
 
 def get_content(url):
     try:
-        response =  requests.get(url)
-        soup = BeautifulSoup(response.text,'html.parser')
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
         headers = []
-        for header_tag in ['h1','h2','h3']:
+        for header_tag in ['h1', 'h2', 'h3']:
             for header in soup.find_all(header_tag):
                 headers.append(header.get_text(strip=True))
         paragraphs = [p.get_text(strip=True) for p in soup.find_all('p')]
-        return jsonify({'heades':headers,'paragraphs':paragraphs})
+        return jsonify({'heades': headers, 'paragraphs': paragraphs})
     except Exception as e:
-        return jsonify({"error":str(e)}),500
+        return jsonify({"error": str(e)}), 500
+
 
 @app.route('/scrape/content', methods=['GET'])
 @auth.login_required
